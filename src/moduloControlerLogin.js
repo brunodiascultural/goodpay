0ec770e0ef274e1e36c6ac085d0c87360b4a5fe4
rdfn37 <rdfn37@gmail.com>
2021-12-23 20:28:13 -0300

    Implementado a lógica de locasStorage

--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -8,7 +8,7 @@ class ControlerUserLogin {
     checkLogin() {
         this.status = true;
     }
-    checkOutLogin(){
+    checkOutLogin() {
         this.status = false;
     }
     loginInfo(user, senha) {
@@ -16,22 +16,25 @@ class ControlerUserLogin {
 
         if (user === null || senha === null || user === "" || senha === "") {
             this.checkOutLogin();
-            localStorage.status=this.status;
+            localStorage.status = this.status;
             return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "camposVazios1"));
         } else {
             if (!infoCheck) {
                 this.checkOutLogin();
-                localStorage.status=this.status;
+                localStorage.status = this.status;
                 return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "usuarioNaoExiste1"));
             } else {
                 if (infoCheck.senha === senha) {
                     this.checkLogin();
-                    localStorage.status=this.status;
+                    localStorage.status = this.status;
+                    localStorage.email = infoCheck.email;
+                    localStorage.userName = infoCheck.username
+                    console.log(localStorage.email);
                     return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "loginExecutado1"));
                 } else {
                     this.checkOutLogin();
-                    localStorage.status=this.status;
-                    return(comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1"));
+                    localStorage.status = this.status;
+                    return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1"));
                 }
             }
         }
