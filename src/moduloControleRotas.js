a4a4bdb6068f33dc35a9b18b9d7c9f3cc1219b87
Igor Pedrosa <ilap1036@gmail.com>
2021-12-26 15:49:40 -0300

    controle de tentativas de login e recaptcha

--- src/moduloControleRotas.js
+++ src/moduloControleRotas.js
@@ -1,19 +1,22 @@
 import rotaApp from "./moduloRotas.js";
 
-class ControleRotasApp {
-    constructor(status, idRota, tela, linkUrl) {
+class ControleRotasApp{
+    constructor(status, idRota, tela, linkUrl){
         this.status = status;
         this.idRota = idRota;
         this.tela = tela;
         this.linkUrl = linkUrl;
     }
-    validaRota(status, idRota) {
-        if (status === "true") {
-            return (rotaApp.find((rotaApp) => rotaApp.idRota === idRota).linkUrl);
+    validaRota(status, idRota){
+        //Ao receber tentativasDemais1 redireciona para o mesma página de falhaLogin1
+        if(idRota === "tentativasDemais1") idRota = "falhaLogin1";
+        if(status === "true"){
+            return (rotaApp.find(i=>i.idRota === idRota).linkUrl);
         } else {
-            return (rotaApp.find((rotaApp) => rotaApp.idRota === idRota).linkUrl);
+            return (rotaApp.find(i=>i.idRota === idRota).linkUrl);
         }
     }
 }
+
 const controleRotasApp = new ControleRotasApp();
 export default controleRotasApp;
--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -4,6 +4,8 @@ import comunicacaoModal from "./moduloComunicacaoDados.js";
 class ControlerUserLogin {
     constructor(status) {
         this.status = status;
+        this.attempts = 0; //Tentativas de login
+        this.resultReference = 0; //Resultado da operação do recaptcha
     }
     checkLogin() {
         this.status = true;
@@ -11,31 +13,51 @@ class ControlerUserLogin {
     checkOutLogin(){
         this.status = false;
     }
-    loginInfo(user, senha) {
-        var infoCheck = userLogin.find((userLogin) => userLogin.username === user);
-
-        if (user === null || senha === null || user === "" || senha === "") {
+    loginInfo(user, senha, recaptcha) {
+        localStorage.clear();
+        let infoCheck = userLogin.find((userLogin) => userLogin.username === user);
+        if (user === null || senha === null || user === "" || senha === "" || recaptcha === null || recaptcha === "") {
             this.checkOutLogin();
-            localStorage.status=this.status;
-            return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "camposVazios1"));
+            localStorage.status = this.status;
+            return comunicacaoModal.find(i => i.idModal === "camposVazios1");
+        } else if (!infoCheck) {
+            this.checkOutLogin();
+            localStorage.status = this.status;
+            return comunicacaoModal.find(i => i.idModal === "usuarioNaoExiste1");
         } else {
-            if (!infoCheck) {
-                this.checkOutLogin();
-                localStorage.status=this.status;
-                return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "usuarioNaoExiste1"));
+            if (infoCheck.senha === senha && recaptcha == this.resultReference) { //Senha e recaptcha precisam estar corretos
+                this.checkLogin();
+                localStorage.status = this.status;
+                return comunicacaoModal.find(i => i.idModal === "loginExecutado1");
             } else {
-                if (infoCheck.senha === senha) {
-                    this.checkLogin();
-                    localStorage.status=this.status;
-                    return (comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "loginExecutado1"));
-                } else {
+                this.attempts++;
+                if(this.attempts !== 3){ //Máximo de tentativas
                     this.checkOutLogin();
-                    localStorage.status=this.status;
-                    return(comunicacaoModal.find((comunicacaoModal) => comunicacaoModal.idModal === "falhaLogin1"));
+                    localStorage.status = this.status;
+                    let tentativasObject = comunicacaoModal.find(i => i.idModal === "tentativasDemais1");
+                    //Passando o resto das tentativas para o usuário
+                    tentativasObject.bodyModal = `Você já fez ${this.attempts} tentativa(s)! Com três será redirecionado(a) para recuperar senha`;
+                    return tentativasObject; 
+                } else {
+                    this.attempts = 0;
+                    this.checkOutLogin();
+                    localStorage.status = this.status;
+                    return comunicacaoModal.find(i => i.idModal === "falhaLogin1");
                 }
             }
         }
     }
+    numberGenerator() { //gerador de números aleatorios para recaptcha
+        let logicalOperator1  = Math.floor(Math.random() * 300);
+        let logicalOperator2  = Math.floor(Math.random() * 10);
+        let result =[{num01: logicalOperator1, 
+                    num02: logicalOperator2, 
+                    sum: logicalOperator1 + logicalOperator2}
+                    ]; 
+        this.resultReference = result[0].sum;
+      return result;
+    }
 }
+
 const controlerUserLogin = new ControlerUserLogin();
 export default controlerUserLogin
