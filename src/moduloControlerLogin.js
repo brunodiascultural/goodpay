c6a248b89d2b782e44e365339fea37e9e9d5ddc1
Lucianocbarbosa <lucianobebarbosa@gmail.com>
2021-12-27 10:25:38 -0300

    Refazendo a PR do squad #39

--- src/moduloControlerLogin.js
+++ src/moduloControlerLogin.js
@@ -10,7 +10,7 @@ class ControlerUserLogin {
     checkLogin() {
         this.status = true;
     }
-    checkOutLogin(){
+    checkOutLogin() {
         this.status = false;
     }
     loginInfo(user, senha, recaptcha) {
@@ -28,16 +28,19 @@ class ControlerUserLogin {
             if (infoCheck.senha === senha && recaptcha == this.resultReference) { //Senha e recaptcha precisam estar corretos
                 this.checkLogin();
                 localStorage.status = this.status;
+                localStorage.email = infoCheck.email;
+                localStorage.userName = infoCheck.username;
+
                 return comunicacaoModal.find(i => i.idModal === "loginExecutado1");
             } else {
                 this.attempts++;
-                if(this.attempts !== 3){ //Máximo de tentativas
+                if (this.attempts !== 3) { //Máximo de tentativas
                     this.checkOutLogin();
                     localStorage.status = this.status;
                     let tentativasObject = comunicacaoModal.find(i => i.idModal === "tentativasDemais1");
                     //Passando o resto das tentativas para o usuário
                     tentativasObject.bodyModal = `Você já fez ${this.attempts} tentativa(s)! Com três será redirecionado(a) para recuperar senha`;
-                    return tentativasObject; 
+                    return tentativasObject;
                 } else {
                     this.attempts = 0;
                     this.checkOutLogin();
@@ -48,14 +51,16 @@ class ControlerUserLogin {
         }
     }
     numberGenerator() { //gerador de números aleatorios para recaptcha
-        let logicalOperator1  = Math.floor(Math.random() * 300);
-        let logicalOperator2  = Math.floor(Math.random() * 10);
-        let result =[{num01: logicalOperator1, 
-                    num02: logicalOperator2, 
-                    sum: logicalOperator1 + logicalOperator2}
-                    ]; 
+        let logicalOperator1 = Math.floor(Math.random() * 300);
+        let logicalOperator2 = Math.floor(Math.random() * 10);
+        let result = [{
+            num01: logicalOperator1,
+            num02: logicalOperator2,
+            sum: logicalOperator1 + logicalOperator2
+        }
+        ];
         this.resultReference = result[0].sum;
-      return result;
+        return result;
     }
 }
 
